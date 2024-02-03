2eca93a9603d1e3f5e92b59308b3b1c72b149a1b
Nicole-Marinho-Espindola <nicole.m.espindola@gmail.com>
2024-02-02 23:47:11 -0300

    starting 'to do list' project

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/global.css">
+    <title>To do list</title>
+</head>
+<body>
+    <div class="title-to-do-list-block">
+        <span class="-title-to-do-list">
+            To do List
+        </span>
+    </div>
+    <div class="card-to-do-list">
+        <div class="card-head">
+            <div class="card-input-block">
+                <input type="text" placeholder="Add a new thing">
+                <i class="fa-solid fa-circle-plus icon"></i>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="card-item-block">
+                <div class="card-item">
+                    <input type="checkbox" name="" id="">
+                    <input type="text">
+                    <i class="fa-solid fa-trash icon"></i>
+                </div>
+                <div class="card-item">
+                    <input type="checkbox" name="" id="">
+                    <input type="text">
+                    <i class="fa-solid fa-trash icon"></i>
+                </div>
+                <div class="card-item">
+                    <input type="checkbox" name="" id="">
+                    <input type="text">
+                    <i class="fa-solid fa-trash icon"></i>
+                </div>
+                <div class="card-item">
+                    <input type="checkbox" name="" id="">
+                    <input type="text">
+                    <i class="fa-solid fa-trash icon"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright-block">
+        <span class="copyright">Feito com <i class="fa-solid fa-heart"></i> por Nicole</span>
+    </div>
+</body>
+</html>
